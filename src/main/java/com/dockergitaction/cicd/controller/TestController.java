b94d743fa885892f3a57217f5100c61bed946fbf
sujeong11 <qalzm7351@gmail.com>
2023-09-01 16:02:23 +0900

    Initial commit

---
+++ src/main/java/com/dockergitaction/cicd/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.dockergitaction.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
